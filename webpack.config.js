cd9085b40e245fa09695642eb8d3ebb1b66428e1
Andrei Sambra <andrei.sambra@akasha.world>
2019-09-06 13:05:02 +0200

    renamed package

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'secure-store.js',
+    filename: 'securestore.js',
     library: 'SecureStore',
     libraryTarget: 'umd',
     umdNamedDefine: true
